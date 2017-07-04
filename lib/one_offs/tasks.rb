c5f53d0ae1144f57f72ceddb44fca744fa629425
Richard Lamb <richard.lamb@silkfred.com>
2017-07-04 13:04:03 +0100

    Modify file generation to use timestamps. Update README

--- lib/one_offs/tasks.rb
+++ lib/one_offs/tasks.rb
@@ -6,16 +6,10 @@ namespace :one_offs do
     # Get command line arguments
     ARGV.each { |a| task a.to_sym do ; end }
 
-    # Get all existing one-off file names
-    one_off_files = Dir.glob("#{Rails.root}/lib/one_offs/**/*").map { |file| file.split('/')[-1] }
-
-    # Calculate increment for new file name
-    increment = one_off_files.any? ? one_off_files.sort.last.scan(/[0-9\(\)]+/)[0].to_i + 1 : 1
-
     directory = Rails.root.join('lib', 'one_offs')
     filename = ARGV[1].singularize
 
-    File.open("#{directory}/#{increment}_#{filename}.rb", "w") do |file|
+    File.open("#{directory}/#{Time.now.to_i}_#{filename}.rb", "w") do |file|
       contents = "class #{filename.classify.to_s}\n"\
                  "  def self.process\n"\
                  "    # Enter your migration code here....\n"\
