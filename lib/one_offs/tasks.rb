afdb145b9de89a1f503c5193e4f5d7aa076b2110
Victor Martinez <vicmargar@gmail.com>
2019-05-10 13:24:12 +0200

    create spec file when creating one_off

--- lib/one_offs/tasks.rb
+++ lib/one_offs/tasks.rb
@@ -1,6 +1,8 @@
 require 'one_offs/runner'
 
 namespace :one_offs do
+  task create: [:create_one_off]
+
   desc "Create a one off script"
   task :create_one_off do
     # Get command line arguments
@@ -8,14 +10,31 @@ namespace :one_offs do
 
     directory = Rails.root.join('lib', 'one_offs')
     filename = ARGV[1]
+    time = Time.now.to_i
 
-    File.open("#{directory}/#{Time.now.to_i}_#{filename}.rb", "w") do |file|
+    File.open("#{directory}/#{time}_#{filename}.rb", "w") do |file|
       contents = "class #{filename.camelize}\n"\
                  "  def self.process\n"\
                  "    # Enter your migration code here....\n"\
                  "  end\n"\
                  "end"
 
+      file.puts(contents)
+    end
+
+    spec_directory = Rails.root.join('spec', 'lib', 'one_offs')
+
+    File.open("#{spec_directory}/#{time}_#{filename}_spec.rb", "w") do |file|
+      contents = <<~CONTENT
+      require 'one_offs/#{time}_#{filename}'
+
+      RSpec.describe #{filename.camelize} do
+        it 'CHANGEME' do
+          expect(1).to eq(2)
+        end
+      end
+
+      CONTENT
       file.puts(contents)
     end
   end
